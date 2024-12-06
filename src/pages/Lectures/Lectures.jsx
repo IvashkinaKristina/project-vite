b995c6ac9a4e350c191a33d7c3fe7092c50ed3d4
AlexeyBruh <bruhov_uch@mail.ru>
2024-12-06 20:26:29 +0500

    Fix
    1

--- src/pages/Lectures/Lectures.jsx
+++ src/pages/Lectures/Lectures.jsx
@@ -1,8 +1,10 @@
 import "./Lectures.css";
+import Menu from "../../GeneralModules/Menu/Menu";
 
 export default function Lectures() {
   return (
     <div class="ContainerForAll">
+      <Menu/>
       <div class="ContentLectures">
         <button class="Content-button-Lectures">ЛЕКЦИЯ №1 «МНОЖЕСТВА»</button>
         <button class="Content-button-Lectures">
